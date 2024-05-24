a47932bda1932d8b3a8507d6e32411cb6f2d4225
tony.origogi <tony.origogi@kakaobrain.com>
2024-05-24 14:40:15 +0900

    바텀 네비게이션 데이터 추가

--- app/src/main/java/com/origogi/pokedex/presentation/components/BottomNavigationBar.kt
+++ app/src/main/java/com/origogi/pokedex/presentation/components/BottomNavigationBar.kt
@@ -9,8 +9,12 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -19,6 +23,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -27,26 +32,92 @@ import com.origogi.pokedex.presentation.theme.DeepSkyBlue
 import com.origogi.pokedex.presentation.theme.PokedexTheme
 
 
-@Composable
+sealed class BottomNavigationButton(
+    val iconOn: Int,
+    val iconOff: Int,
+    val label: String
+) {
+    object Pokedex : BottomNavigationButton(
+        iconOn = R.drawable.icon_pokedex_on,
+        iconOff = R.drawable.icon_pokedex_off,
+        label = "Pokedéx"
+    )
 
-fun BottomNavigationBar() {
+    object Region : BottomNavigationButton(
+        iconOn = R.drawable.icon_region_on,
+        iconOff = R.drawable.icon_region_off,
+        label = "Region"
+    )
+
+    object Favorite : BottomNavigationButton(
+        iconOn = R.drawable.icon_favorite_on,
+        iconOff = R.drawable.icon_favorite_off,
+        label = "Favorite"
+    )
+
+    object Profile : BottomNavigationButton(
+        iconOn = R.drawable.icon_profile_on,
+        iconOff = R.drawable.icon_profile_off,
+        label = "Profile"
+    )
 
 }
 
 @Composable
-fun BottomNavigationBarItem(isSelected: Boolean) {
+
+fun BottomNavigationBar() {
+
+    val buttons = listOf(
+        BottomNavigationButton.Pokedex,
+        BottomNavigationButton.Region,
+        BottomNavigationButton.Favorite,
+        BottomNavigationButton.Profile
+    )
+
+    var selected by remember {
+        mutableStateOf(0)
+    }
+
+    NavigationBar(
+        containerColor = Color.White,
+        contentColor = Color.White,
+        modifier = Modifier.fillMaxWidth(),
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth().padding(8.dp),
+            horizontalArrangement = Arrangement.SpaceAround,
+        ) {
+            buttons.forEachIndexed { index, button ->
+                Box(Modifier.clickable {
+                    selected = index
+                }) {
+                    BottomNavigationBarItem(
+                        isSelected = index == selected,
+                        iconOn = button.iconOn,
+                        iconOff = button.iconOff,
+                        label = button.label
+                    )
+                }
+            }
+        }
+
+    }
+}
+
+@Composable
+fun BottomNavigationBarItem(isSelected: Boolean, iconOn: Int, iconOff: Int, label: String) {
     Column(
         Modifier.size(56.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
 
-        Crossfade(targetState = isSelected, label = "icon crossfade",) {
-            val icon = if (it) R.drawable.icon_pokedex_on else R.drawable.icon_pokedex_off
+        Crossfade(targetState = isSelected, label = "icon crossfade $label") {
+            val icon = if (it) iconOn else iconOff
 
             Image(
                 painter = painterResource(id = icon),
-                contentDescription = "Pokedéx",
+                contentDescription = label,
                 modifier = Modifier.size(24.dp)
             )
         }
@@ -55,7 +126,7 @@ fun BottomNavigationBarItem(isSelected: Boolean) {
 
             ) {
             Text(
-                "Pokedéx",
+                label,
                 style = MaterialTheme.typography.labelSmall,
                 color = DeepSkyBlue
             )
@@ -71,13 +142,26 @@ fun BottomNavigationBarItemPreview() {
         mutableStateOf(false)
     }
 
+    val button = BottomNavigationButton.Region
+
     PokedexTheme {
         Box(modifier = Modifier.clickable {
             state = !state
         }) {
-            BottomNavigationBarItem(state)
+            BottomNavigationBarItem(
+                state,
+                iconOn = button.iconOn,
+                iconOff = button.iconOff,
+                label = button.label
+            )
         }
+    }
+}
 
-
+@Preview(showBackground = true)
+@Composable
+fun BottomNavigationBarPreview() {
+    PokedexTheme {
+        BottomNavigationBar()
     }
 }
--- app/src/main/java/com/origogi/pokedex/presentation/screen/MainScreen.kt
+++ app/src/main/java/com/origogi/pokedex/presentation/screen/MainScreen.kt
@@ -7,16 +7,23 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.origogi.pokedex.presentation.components.BottomNavigationBar
 import com.origogi.pokedex.presentation.theme.PokedexTheme
 
 @Composable
 fun MainScreen() {
-    Scaffold {
-        Surface(Modifier.padding(it)) {
-            Text(text = " Hello Pokedex!")
-
+    Scaffold(
+        content = {
+            Surface(
+                modifier = Modifier.padding(it),
+            ) {
+                Text("Hello Pokedex")
+            }
+        },
+        bottomBar = {
+             BottomNavigationBar()
         }
-    }
+    )
 }
 
 @Preview
