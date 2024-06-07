7274bae30bdeb7aab4b86500d557e17742616bb4
tony.origogi <tony.origogi@kakaobrain.com>
2024-06-07 16:13:07 +0900

    리팩토링

--- app/src/main/java/com/origogi/pokedex/data/repository/PokemonWeaknessTypesRepositoryImpl.kt
+++ app/src/main/java/com/origogi/pokedex/data/repository/PokemonWeaknessTypesRepositoryImpl.kt
@@ -6,27 +6,38 @@ import com.origogi.pokedex.domain.repository.PokemonWeaknessTypesRepository
 import com.origogi.pokedex.extenstion.capitalizeFirstChar
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 class PokemonWeaknessTypesRepositoryImpl @Inject constructor(
     private val client: PokedexApiClient
 ) : PokemonWeaknessTypesRepository {
 
-    private val cache = mutableMapOf<PokemonType, List<PokemonType>>()
-    override fun get(type: PokemonType): Flow<List<PokemonType>> = flow {
-        if (!cache.containsKey(type)) {
+    private val _cache = MutableStateFlow<Map<PokemonType, List<PokemonType>>>(emptyMap())
+    private val cache: StateFlow<Map<PokemonType, List<PokemonType>>> = _cache
 
-            val data = client.fetchPokemonTypeDetailData(type.name.lowercase())
-            val weaknessTypes = data.damageRelations.doubleDamageFrom.map { it.name }.map {
-                PokemonType.valueOf(it.capitalizeFirstChar())
-            }
-            cache[type] = weaknessTypes
+    override fun get(type: PokemonType): Flow<List<PokemonType>> {
+        return cache.map {
+            it[type] ?: emptyList()
         }
-        emit(cache[type]!!)
-
-    }.flowOn(
-        Dispatchers.IO
-    )
+            // If the cache doesn't have the data, fetch it from the network
+            .map { weaknessTypes ->
+                weaknessTypes.ifEmpty {
+                    val typeData =
+                        client.fetchPokemonTypeDetailData(type.name.lowercase())
+                    val types = typeData.damageRelations.doubleDamageFrom.map {
+                        PokemonType.valueOf(it.name.capitalizeFirstChar())
+                    }
+                    _cache.update {
+                        it + (type to types)
+                    }
+                    types
+                }
+            }
+            .flowOn(Dispatchers.IO)
+    }
 }
